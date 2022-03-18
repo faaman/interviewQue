2f81b6d7c7e84566c0037b94bdf0f0aa20e2db00
faaman <faaman2020@gmail.com>
2022-03-18 09:25:14 -0700

    updates to script

--- logParse.py
+++ logParse.py
@@ -2,25 +2,30 @@
 # A test suite must be included
 # hij: use regex module
 # do not use the 'head', 'tail' or 'grep' utilities
-# options needed: h
-# options needed: -f (--first NUM, Print first NUM lines)
+# options needed: -h
+# options needed: -f (--first NUM, Print first NUM lines) - if this is chosen, NUM is mandatory?
 # options needed: -l (--last NUM, Print last NUM lines)
 # options needed: -t (--timestamps, Print lines that contain a timestamp in HH:MM:SS format)
 # options needed: -i (--ipv4, Print lines that contain an IPv4 address, matching IPs are highlighted)
 # options needed: -I (--ipv6, Print lines that contain an IPv6 address, std notation, matching IPs are highlighted)
+# If FILE is omitted, standard input is used instead
+# Usage: ./util.py [OPTION]... [FILE]
 
 from sys import argv
 import re
 
 totalArgs = len(argv)
+numLines = 0
+optionList = []
 
 script = argv[0]
-filename = argv[1]
-option = argv[2]
-if totalArgs == 4:
-    numLines = argv[3]
-else:
-    numLines = 0
+
+i = 1
+for k in range(0, totalArgs - 2):
+    optionList.append(argv[i])
+    i = i+1
+
+filename = argv[totalArgs - 1]
 
 ########################
 
@@ -30,53 +35,63 @@ with open(filename, 'r') as fp:
         pass
 print('Total Lines', count + 1)
 
-txt = open(filename)
 
-# first lines option
-if option == '-f' or option == '--first':
-    print("These are the first required lines from the file: ")
-    lines = txt.readlines()
-    for i in range(0, int(numLines)):
-        print(lines[i])
+for i in range(0, totalArgs-2):
 
-# last lines option
-if option == '-l' or option == '--last':
-    print("These are the last required lines from the file: ")
-    lines = txt.readlines()
-    startHere = (count+1)-int(numLines)
-    for i in range(startHere, count+1):
-        print(lines[i])
+    # first lines option
+    if optionList[i] == '-f' or optionList[i] == '--first':
+        txt = open(filename)
+        numLines = optionList[i+1]
+        print("These are the first", int(numLines), "lines from the file: ")
+        lines = txt.readlines()
+        for j in range(0, int(numLines)):
+            print(lines[j])
+        txt.close()
 
-# find all timestamps
-if option == '-t' or option == '--timestamps':
-    print("These are the lines from the file that contain timestamps: ")
-    lines = txt.readlines()
-    for i in range(0, count):
-        #if re.match('\d{2}:\d{2}:\d{2}', lines[i]):
-        # The previous line has the limitation that it only shows true if the timestamp is at the beginning of line
-        if re.search('\d{2}:\d{2}:\d{2}', lines[i]):
-            print("At line: %i" % i)
-            print(lines[i])
+    # last lines option
+    if optionList[i] == '-l' or optionList[i] == '--last':
+        txt = open(filename)
+        numLines = optionList[i+1]
+        print("These are the last", int(numLines), "lines from the file: ")
+        lines = txt.readlines()
+        startHere = (count+1)-int(numLines)
+        for j in range(startHere, count+1):
+            print(lines[j])
+        txt.close()
 
-
-if option == '-i' or option == '--ipv4':
-    print("These are the lines from the file that contain an IPv4 address: ")
-    lines = txt.readlines()
-    pattern = r"((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])[ (\[]?(\.|dot)[ )\]]?){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))"
-    for i in range(0, count):
-        if re.search(pattern, lines[i]):
-            print("At line: %i" % i)
-            print(lines[i])
-
-if option == '-I' or option == '--ipv6':
-    print("These are the lines from the file that contain an IPv6 address: ")
-    lines = txt.readlines()
-    pattern = r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}| ([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:) {1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
-    for i in range(0, count):
-        if re.search(pattern, lines[i]):
-            print("At line: %i" % i)
-            print(lines[i])
+    # find all timestamps
+    if optionList[i] == '-t' or optionList[i] == '--timestamps':
+        txt = open(filename)
+        print("These are the lines from the file that contain timestamps: ")
+        lines = txt.readlines()
+        print(count)
+        for m in range(0, count):
+            #if re.match('\d{2}:\d{2}:\d{2}', lines[i]):
+            # The previous line has the limitation that it only shows true if the timestamp is at the beginning of line
+            if re.search('\d{2}:\d{2}:\d{2}', lines[m]):
+                print("At line: %i" % m)
+                print(lines[m])
+        txt.close()
 
 
-# Close the file after all usage
-txt.close()
+    if optionList[i] == '-i' or optionList[i] == '--ipv4':
+        txt = open(filename)
+        print("These are the lines from the file that contain an IPv4 address: ")
+        lines = txt.readlines()
+        pattern = r"((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])[ (\[]?(\.|dot)[ )\]]?){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))"
+        for j in range(0, count):
+            if re.search(pattern, lines[j]):
+                print("At line: %i" % j)
+                print(lines[j])
+        txt.close()
+
+    if optionList[i] == '-I' or optionList[i] == '--ipv6':
+        txt = open(filename)
+        print("These are the lines from the file that contain an IPv6 address: ")
+        lines = txt.readlines()
+        pattern = r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}| ([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:) {1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
+        for j in range(0, count):
+            if re.search(pattern, lines[j]):
+                print("At line: %i" % j)
+                print(lines[j])
+        txt.close()
