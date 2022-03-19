be5e7fcb0ffd98389384afa37d583751c59f0866
faaman <faaman2020@gmail.com>
2022-03-18 17:39:06 -0700

    corrected standard input issues

--- logParse.py
+++ logParse.py
@@ -15,12 +15,12 @@
 from sys import argv
 import re
 import datetime
-import decimal
+#import decimal
 
 totalArgs = len(argv)
 numLines = 0
 optionList = []
-
+count = 0
 script = argv[0]
 
 i = 1
@@ -28,19 +28,33 @@ for k in range(0, totalArgs - 2):
     optionList.append(argv[i])
     i = i+1
 
-filename = argv[totalArgs - 1]
-
 ########################
 
-# find out total number of lines in file
-try:
-    with open(filename, 'r') as fp:
-        for count, line in enumerate(fp):
-            pass
-    print('Total Lines', count + 1)
-except FileNotFoundError:
-    print("There is no valid file mentioned.")
-    exit()
+# find out total number of lines in file, if file does not exist, get standard input from user
+def catchingNoFiles():
+    try:
+        filename = argv[totalArgs - 1]
+        with open(filename, 'r') as fp:
+            for count, line in enumerate(fp):
+                pass
+        print('Total Lines', count + 1)
+        return filename, count
+    except FileNotFoundError:
+        print("There is no valid file mentioned.")
+        linesCmd = input("How many lines of text would you like to input? ")
+        target = open('standardIpText.txt', 'w')
+        line1 = input("Input line: ")
+        target.write(line1)
+        for li in range(1, int(linesCmd)):
+            line1 = input("Input line: ")
+            target.write("\n")
+            target.write(line1)
+        target.close()
+        count = int(linesCmd) - 1
+        filename = 'standardIpText.txt'
+        print('Total Lines', int(count) + 1)
+        return filename, count
+
 
 # a loop for all the options the user has
 
@@ -54,7 +68,11 @@ for i in range(0, totalArgs-2):
 
     # first lines option
     if optionList[i] == '-f' or optionList[i] == '--first':
+        filename, count = catchingNoFiles()
         txt = open(filename)
+        # the below is an abrupt ending
+        if int(count) < int(optionList[i+1]):
+            exit()
         try:
             if int(optionList[i+1]) >= 0 or int(optionList[i+1]) <= count:
                 numLines = optionList[i+1]
@@ -72,7 +90,11 @@ for i in range(0, totalArgs-2):
 
     # last lines option
     if optionList[i] == '-l' or optionList[i] == '--last':
+        filename, count = catchingNoFiles()
         txt = open(filename)
+        # the below is an abrupt ending
+        if int(count) < int(optionList[i+1]):
+            exit()
         try:
             if int(optionList[i+1]) >= 0 or int(optionList[i+1]) <= count:
                 numLines = optionList[i+1]
@@ -84,19 +106,18 @@ for i in range(0, totalArgs-2):
             numLines = 0
         print("These are the last", int(numLines), "lines from the file: ")
         lines = txt.readlines()
-        startHere = (count+1)-int(numLines)
-        for j in range(startHere, count+1):
+        startHere = int(count)+1 - int(numLines)
+        for j in range(startHere, int(count)+1):
             print(lines[j])
         txt.close()
 
     # find all timestamps
     if optionList[i] == '-t' or optionList[i] == '--timestamps':
+        filename, count = catchingNoFiles()
         txt = open(filename)
         print("These are the lines from the file that contain timestamps: ")
         lines = txt.readlines()
-        for m in range(0, count):
-            #if re.match('\d{2}:\d{2}:\d{2}', lines[i]):
-            # The previous line has the limitation that it only shows true if the timestamp is at the beginning of line
+        for m in range(0, int(count)+1):
             if re.search('\d{2}:\d{2}:\d{2}', lines[m]):
                 try:
                     myDate = re.findall('\d{2}:\d{2}:\d{2}', lines[m])
@@ -110,7 +131,7 @@ for i in range(0, totalArgs-2):
                     if (int(tempHr[0])) >= 0 and (int(tempHr[0])) <= 24:
                         if (int(tempMi[0])) >= 0 and (int(tempMi[0])) <= 59:
                             if (int(tempSe[0])) >= 0 and (int(tempSe[0])) <= 59:
-                                print("At line: %i" % m)
+                                print("At line: ", m+1)
                                 print(lines[m])
 
                 except ValueError:
@@ -121,23 +142,25 @@ for i in range(0, totalArgs-2):
 
 
     if optionList[i] == '-i' or optionList[i] == '--ipv4':
+        filename, count = catchingNoFiles()
         txt = open(filename)
         print("These are the lines from the file that contain an IPv4 address: ")
         lines = txt.readlines()
         pattern = r"((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])[ (\[]?(\.|dot)[ )\]]?){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))"
-        for j in range(0, count):
+        for j in range(0, int(count)+1):
             if re.search(pattern, lines[j]):
-                print("At line: %i" % j)
+                print("At line: ", j+1)
                 print(lines[j])
         txt.close()
 
     if optionList[i] == '-I' or optionList[i] == '--ipv6':
+        filename, count = catchingNoFiles()
         txt = open(filename)
         print("These are the lines from the file that contain an IPv6 address: ")
         lines = txt.readlines()
         pattern = r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}| ([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:) {1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
-        for j in range(0, count):
+        for j in range(0, int(count)+1):
             if re.search(pattern, lines[j]):
-                print("At line: %i" % j)
+                print("At line: ", j+1)
                 print(lines[j])
         txt.close()
